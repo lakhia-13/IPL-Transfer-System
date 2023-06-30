295dd810967476fa050e356bef09d137be81e3eb
Sandipsinh Rathod <zotbysandip@gmail.com>
2023-06-30 22:15:43 +0530

    bug fixes

--- ipl.c
+++ ipl.c
@@ -46,8 +46,8 @@ void insert_team(char *t, char *men, int gw, int gl, char *cname, float sal){
 void insert_plyr(char* team, char* playr, float sal){
     tm* temp = head;
     plyr* currentp = malloc(sizeof(plyr));
-    while (temp->next_team!=NULL){
-        if (strcmp(team, temp->tname)==0){
+    while (temp->next_team){
+        if (!strcmp(team, temp->tname)){
             break;
         }
         temp = temp->next_team;
@@ -56,14 +56,27 @@ void insert_plyr(char* team, char* playr, float sal){
     while(curp->next_player){
         curp = curp->next_player;
     }
-    strncpy(currentp->pname,playr,str_len);
-    currentp->pname[str_len-1] = '\0';
+    strcpy(currentp->pname,playr);
     currentp->salary = sal;
     currentp->next_player = NULL;
     curp->next_player = currentp;
 }
 
-void traverse_team(tm* head){
+void traverse_player(char* team){
+    tm* temp = head;
+    while(temp){
+        if (!strcmp(team, temp->tname)) break;
+        temp = temp->next_team;
+    }
+    plyr* cup = temp->node_p->next_player;
+    while(cup){
+        printf("Player : %s\n", cup->pname);
+        printf("Salary : %f\n\n", cup->salary);
+        cup = cup->next_player;
+    }
+}
+
+void traverse_team(){
     tm *temp = head;
     while(temp != NULL){
         printf("Team : %s\n", temp->tname);
@@ -74,20 +87,7 @@ void traverse_team(tm* head){
         printf("Games lost : %d\n\n", temp->games_lost);
         temp = temp->next_team;
     }
-};
-
-void traverse_player(tm* head, char* team){
-    tm* temp = head;
-    while(temp != NULL){
-        if (strcmp(team, temp->tname)==0){break;}
-    }
-    plyr* cup = temp->node_p->next_player;
-    while(cup!=NULL){
-        printf("Player : %s\n", cup->pname);
-        printf("Salary : %f\n\n", cup->salary);
-        cup = cup->next_player;
-    }
-};
+}
 
 void del_middle_team(tm* head, char* x){
     tm* temp = head;
@@ -106,12 +106,16 @@ void get_player();
 int main(){
     insert_team("Gujarat Titans", "Ashish Nehra", 10, 1, "Hardik Pandya", 1000000);
     insert_team("Mumbai Indians", "Sachin Tendulkar", 20, 3, "Kevin Pollard", 2000000);
+
     insert_plyr("Gujarat Titans", "Shubhman Gill", 2000000);
     insert_plyr("Gujarat Titans", "Abhinav Malohar", 2000000);
     insert_plyr("Gujarat Titans", "Sai Sudarshan", 2000000);
     insert_plyr("Gujarat Titans", "David Miller", 2000000);
     insert_plyr("Gujarat Titans", "Dasun Shanaka", 2000000);
-    traverse_team(head);
-    traverse_player(head, "Gujarat Titans");
+    insert_plyr("Mumbai Indians", "Foo Bar", 2000000);
+    insert_plyr("Mumbai Indians", "Foo1 Bar1", 2000000);
+    traverse_team();
+//    traverse_player("Gujarat Titans");
+    traverse_player("Mumbai Indians");
     return 0;
 }
